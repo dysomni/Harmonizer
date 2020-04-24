a1e85360b92d39d0750034f22fc8ca4274abb896
james <dev@dysomni.com>
2020-04-24 05:41:16 -0400

    added smoothing between pitch changes and shifter activations

--- Source/PitchShifter.cpp
+++ Source/PitchShifter.cpp
@@ -16,8 +16,27 @@ PitchShifter::~PitchShifter(){
 }
 
 
-float PitchShifter::processSample(float x){
+float PitchShifter::processSample(float x) {
+    x = x * changeGain;
+    float x1 = pitchDelay1.processSample(x, a1);
+    float x2 = pitchDelay2.processSample(x, a2);
+    float x3 = pitchDelay3.processSample(x, a3);
     
+    float g1 = calcGain(a1);
+    float g2 = calcGain(a2);
+    float g3 = calcGain(a3);
+    
+    validateAngleBounds(a1);
+    validateAngleBounds(a2);
+    validateAngleBounds(a3);
+    
+    validateChange(true);
+    
+    return (g1*x1 + g2*x2 + g3*x3) * (2.f/3.f) * changeGain;
+}
+
+float PitchShifter::kindaProcessSample(float x) {
+    x = x * changeGain;
     
     float x1 = pitchDelay1.processSample(x, a1);
     float x2 = pitchDelay2.processSample(x, a2);
@@ -31,7 +50,41 @@ float PitchShifter::processSample(float x){
     validateAngleBounds(a2);
     validateAngleBounds(a3);
     
-    return (g1*x1 + g2*x2 + g3*x3) * (2.f/3.f);
+    validateChange(false);
+    
+    return (g1*x1 + g2*x2 + g3*x3) * (2.f/3.f) * changeGain;
+}
+
+void PitchShifter::validateChange(bool on) {
+    if (!on) {
+        if (changeGain == 0.f && changeGainInc == 0.f) return;
+        changeGainInc = -GAIN_CHANGE_SPEED;
+        changeGain += changeGainInc;
+        if(changeGain <= 0.f) {
+            changeGain = 0.f;
+            changeGainInc = 0.f;
+        }
+    } else {
+        if (changeGainInc < 0) {
+            changeGain += changeGainInc;
+            if(changeGain <= 0.f) {
+                changeGain = 0.f;
+                changeGainInc = GAIN_CHANGE_SPEED;
+                actualSetPitch(pitchBuffer);
+            }
+        } else if (changeGainInc > 0) {
+            changeGain += changeGainInc;
+            if(changeGain >= 1.f) {
+                changeGain = 1.f;
+                changeGainInc = 0.f;
+            }
+        } else if (changeGainInc == 0.f) {
+            if (changeGain == 0.f) {
+                changeGainInc = GAIN_CHANGE_SPEED;
+                changeGain += changeGainInc;
+            }
+        }
+    }
 }
 
 void PitchShifter::setFs(float Fs){
@@ -43,8 +96,13 @@ void PitchShifter::setFs(float Fs){
     calcAngleChange();
 }
 
+void PitchShifter::setPitch(float semitone) {
+    if (this->semitone == semitone) return;
+    pitchBuffer = semitone;
+    changeGainInc = -GAIN_CHANGE_SPEED;
+}
 
-void PitchShifter::setPitch(float semitone){
+void PitchShifter::actualSetPitch(float semitone) {
     this->semitone = semitone;
     
     calcDelta();
